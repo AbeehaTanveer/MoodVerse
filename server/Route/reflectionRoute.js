43cc7c45e368fa18b82523c0d8a9fb9b332ad2e1
Abeeha Zainab <abeehazainab25@gmail.com>
2025-08-16 18:01:12 +0500

    Fix case sensitivity for middleware import

--- server/Route/reflectionRoute.js
+++ server/Route/reflectionRoute.js
@@ -2,7 +2,8 @@ const express = require("express");
 const refelctionRoute = express.Router();
 
 const { getReflections, createReflection } = require("../Controller/reflectionController");
-const verifyToken = require("../Middleware/authMiddleware");
+const verifyToken = require("../middleware/authmiddleware");
+
 
 refelctionRoute.get("/", verifyToken, getReflections);
 refelctionRoute.post("/", verifyToken, createReflection);
